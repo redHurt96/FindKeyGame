a51f251980c812e20ab0b3bfcaaa22a2fecfe8ca
redHurt <redhead112358@gmail.com>
2022-02-19 20:40:27 +0300

    Add collect fx

--- FindKey/Assets/_FindKey/Scripts/MonoBehaviours/CollectKeyFx.cs
+++ FindKey/Assets/_FindKey/Scripts/MonoBehaviours/CollectKeyFx.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using AP.FindKey.Common;
 using UnityEngine;
 
-public class CollectKeyFx : MonoBehaviour
+namespace AP.FindKey.MonoBehaviours
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CollectKeyFx : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private GameObject _fxPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start() =>
+            GlobalEvents.KeyFounded.AddListener(CreateFx);
+
+        private void OnDestroy() =>
+            GlobalEvents.KeyFounded.RemoveListener(CreateFx);
+
+        private void CreateFx() =>
+            Instantiate(_fxPrefab, transform);
     }
 }
